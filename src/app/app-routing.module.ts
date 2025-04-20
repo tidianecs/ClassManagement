8ad8c39626b4f7fec6c405d5b27a5bb62877a534
Tidiane <ccheikh687pgmail.com>
2025-04-20 02:18:17 +0000

    I just make the front of the signup page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'signUp', component: SignUpComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SignUpComponent } from './sign-up/sign-up.component';
+
+
+
+@NgModule({
+  declarations: [
+    SignUpComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AuthModule { }
